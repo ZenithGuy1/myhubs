c5926a85ba5149bc1b842d7f420632677bdc55b0
ZenithGuy1 <147445671+ZenithGuy1@users.noreply.github.com>
2023-11-26 14:53:26 -0300

    Update ArmWrestlingSim.lua

--- ArmWrestlingSim.lua
+++ ArmWrestlingSim.lua
@@ -4,7 +4,7 @@ end
 
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/ionlyusegithubformcmods/1-Line-Scripts/main/Mobile%20Friendly%20Orion')))()
 
-local Window = OrionLib:MakeWindow({Name = "Numerous Hub | Blade Ball", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest",IntroText = "Numerous Blade",IntroIcon = "rbxassetid://7733678388"})
+local Window = OrionLib:MakeWindow({Name = "Sitzo Hub | Arm Wrestling Simulator", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest",IntroText = "Sitzo Hub | AWS",IntroIcon = "rbxassetid://7733678388"})
 
 local Tab = Window:MakeTab({
     Name = "Main",
@@ -12,5 +12,25 @@ local Tab = Window:MakeTab({
     PremiumOnly = false
   })
 
+local Tab2 = Window:MakeTab({
+        Name = "Farming",
+        Icon = "rbxassetid://7733674153",
+        PremiumOnly = false
+    })
 
+Tab:AddLabel("Script by @zenithguy")
+
+Tab:AddButton({
+        Name = "Infinite Yield",
+        Callback = function()
+            loadstring(game:HttpGet('https://raw.githubusercontent.com/ionlyusegithubformcmods/1-Line-Scripts/main/Infinite%20Yield%20but%20with%20secure%20dex'))()
+        end
+    })
+
+Tab:AddButton({
+        Name = "Destroy GUI",
+        Callback = function()
+            OrionLib:Destroy()
+        end
+    })
 OrionLib:Init()
