76b946f9980046686df045627bc0f1076d3ef2c4
ZenithGuy1 <147445671+ZenithGuy1@users.noreply.github.com>
2023-12-17 09:49:01 -0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -21,6 +21,8 @@ MakeWindow({
   })
 
 local Main = MakeTab({Name = "The main"})
+local item = MakeTab({Name = "Uhh.. items."})
+
 local Section = AddSection(Main, {"Teste"})
 
 AddButton(Main, {
@@ -30,3 +32,20 @@ AddButton(Main, {
     end
 })
 
+AddButton(item, {
+    Name = "item",
+    Callback = function()
+      local tool = Instance.new("Tool")
+tool.Name = "se eu fosse tu abaixava o volume"
+tool.RequiresHandle = false
+tool.Parent = game.Players.LocalPlayer.Backpack
+tool.Activated:connect(function()
+local sound = Instance.new("Sound")
+sound.SoundId = "rbxassetid://8280196339"
+sound.Parent = game.Players.LocalPlayer
+sound.Volume = 1
+sound:Play()
+wait(2.3)
+game.Players.LocalPlayer.Character:BreakJoints()
+end)
+    end
