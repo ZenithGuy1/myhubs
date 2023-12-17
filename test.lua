1d969da281527236d7c2e365f3464e06e862ae61
ZenithGuy1 <147445671+ZenithGuy1@users.noreply.github.com>
2023-12-17 09:06:47 -0300

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,21 @@
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/REDzHUB/LibraryV2/main/redzLib")))()
+
+MakeWindow({
+    Hub = {
+        Title = "Zenith Hub",
+        Animation = "by @zenithguy"
+    },
+    Key = {
+      KeySytem = true,
+      Title = "Key System",
+      Description = "",
+      KeyLink = "",
+      Keys = {"zenithguy"},
+      Notifi = {
+        Notifications = true,
+        CorrectKey = "Correct Key!",
+        IncorrectKey = "Invalid Key!",
+        CopyKeyLink = "Copied to your clipboard"
+      }
+    }
+  })
