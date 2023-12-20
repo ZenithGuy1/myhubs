f98ae1cf61d9ef16ebb08fb49a2f794e693a702b
ZenithGuy1 <147445671+ZenithGuy1@users.noreply.github.com>
2023-12-20 12:12:43 -0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,5 +1,18 @@
 loadstring(game:HttpGet(("https://raw.githubusercontent.com/REDzHUB/LibraryV2/main/redzLib")))()
 
+local Scream = Instance.new("Sound")
+    Scream.SoundId = "rbxassetid://8280196339"
+    Scream.Parent = game.SoundService
+
+function scream()
+local STool = Instance.new("Tool")
+    Stool.Name = "Turn down the volume!"
+    Stool.Parent = game.Players.LocalPlayer.Backpack
+    STool.Activated:connect(function()
+        Scream:Play()
+        end)
+    end
+
 MakeWindow({
         Hub = {
             Title = "Numerous Hub",
@@ -21,3 +34,14 @@ MakeWindow({
 })
 
 local Main = MakeTab({Name = "Main"})
+
+local Items = MakeTab({Name = "Weird Items"})
+
+local Functions = MakeTab({Name = "Player Functions"})
+
+AddButton(Items, {
+        Name = "scream",
+        Callback = function()
+                scream()
+            end
+        })
