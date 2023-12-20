66acf64731b017032e02f7853560d2cb9c71c089
ZenithGuy1 <147445671+ZenithGuy1@users.noreply.github.com>
2023-12-19 22:10:28 -0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,3 +1,17 @@
 local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/Redzlib/main/Source.lua"))()
 
 local Window = redzlib:MakeWindow({
+    Menu = {
+      Title = "test",
+    },
+    LoadAnim = {
+      Active = true,
+      Title = "bom dia",
+      WaitTime = 0.7
+    }
+  })
+
+local Tab = Window:MakeTab({
+    Name = "test",
+    Icon = "rbxassetid://7743877573"
+  })
