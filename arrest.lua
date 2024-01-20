db4649b6d91f8328bc39d61392faa6fc34fdb90e
ZenithGuy1 <147445671+ZenithGuy1@users.noreply.github.com>
2024-01-20 17:48:54 -0300

    Create arrest.lua

---
+++ arrest.lua
@@ -0,0 +1,15 @@
+_G.arest = true
+while _G.arest do
+  wait(0.2)
+  local plr = game.Players.LocalPlayer
+  local players = game.Players:GetPlayers()
+  for _, oder in pairs(players) do
+  if oder ~= plr then
+      local distance = (plr.Character.HumanoidRootPart.Position - oder.Character.HumanoidRootPart.Position).Magnitude
+      if distance < 17 then
+        local args = {[1] = oder.Character.Head}
+        workspace.Remote.arrest:InvokeServer(unpack(args))
+      end
+    end
+  end
+end
